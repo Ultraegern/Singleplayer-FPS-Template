0984f5dc4f62d1c046a714135ce746d6d798f2a3
Ultraegern <155380537+Ultraegern@users.noreply.github.com>
2025-08-23 20:22:09 +0200

    Add player character and update world scene
    Introduces a new player character with a capsule mesh, collision, neck, and camera. Updates the world scene to instance the player and removes the old static camera. Also updates mesh import settings and unwrap caches for Box and Stair assets, and adjusts mesh library resources for improved light baking and lightmap hints.

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,19 +1,38 @@
 extends CharacterBody3D
+class_name Player
+
+const SPEED: float = 5.0
+const JUMP_VELOCITY: float = 4.5
+
+var sensitivity: float = 0.005
+
+@onready var camera: Camera3D = $Neck/Camera3D
 
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+func _unhandled_input(event: InputEvent) -> void:
+	#Camera control
+	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+		rotate_y(-event.relative.x * sensitivity)
+		camera.rotate_x(-event.relative.y * sensitivity)
+	camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-90.0), deg_to_rad(90.0))
+	
+	#Mouse capturing
+	if Input.is_action_just_pressed("ui_cancel"):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	if event is InputEventMouseButton:
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 
 func _physics_process(delta: float) -> void:
+	#Movement
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 	
-	var input_dir := Input.get_vector("Left", "Right", "Forward", "Backward")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var input_dir: Vector2 = Input.get_vector("Left", "Right", "Forward", "Backward")
+	var direction: Vector3 = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
